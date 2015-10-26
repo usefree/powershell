bab2b524c641f3cf721ccc0def4391ca8ee8093f
usefree <usefreeit@gmail.com>
2015-10-26 16:05:09 +0200

    Second commit

---
+++ Check_volumes_free_space.ps1
@@ -0,0 +1,66 @@
+param([int]$w=10, [int]$c=5)
+$Status = 0
+$DefaultMessage = "Ok, Free space on all volumes more than 10%"
+$Message = $DefaultMessage
+$VolumesNeedCriticalAttention = ""
+$VolumesNeedWarningAttention = ""
+$ListCSV = ""
+# --- Disks free space check -----
+$Hostname = hostname
+$Wmiq = 'SELECT * FROM Win32_LogicalDisk WHERE Size != Null AND DriveType >= 2'
+$Disks = Get-WmiObject -Query $Wmiq -ErrorAction SilentlyContinue -ErrorVariable ProcessError;
+if ($ProcessError) {
+	$Status = 3
+	$Message = "Unknown disk check error!"
+} else {
+	foreach ($Disk in $Disks) {
+		$CustomDiskPercentFreeSpace = [int](($Disk.FreeSpace)/($Disk.Size) * 100)
+		if (($CustomDiskPercentFreeSpace -le $c) -and ($Disk.FreeSpace -le 1073741824)) {
+			if (($Message -eq $DefaultMessage) -or ($Message -eq "Warning")) {
+				$Message = "Critical"
+				$Status = 2
+			}
+			$VolumesNeedCriticalAttention += "Disk: $($Disk.DeviceID)\ Free Space: $CustomDiskPercentFreeSpace%; "
+		} elseif (($CustomDiskPercentFreeSpace -le $w) -and ($Disk.FreeSpace -le 2147483648)) {
+			if ($Message -eq $DefaultMessage) {
+				$Message = "Warning"
+				$Status = 1
+			}
+			$VolumesNeedWarningAttention += "Disk: $($Disk.DeviceID)\ Free Space: $CustomDiskPercentFreeSpace%; "
+		}
+	}
+}
+# ----- Cluster shared volume free space check ----
+
+Import-Module FailoverClusters
+$ClasterVolumes = Get-ClusterSharedVolume -ErrorAction SilentlyContinue -ErrorVariable ProcessError;
+if ($ProcessError) {
+	$ListCSV = "Host have no Fail Over Cluster Role."
+} else {
+	foreach ($Volume in $ClasterVolumes) {
+		if ($Hostname -eq $Volume.OwnerNode) {
+			$OwnerNode = $Volume.OwnerNode
+			$FriendlyVolumeName = $Volume.SharedVolumeInfo.FriendlyVolumeName
+			$CustomVolumePercentFree = [int]($Volume.SharedVolumeInfo.Partition.PercentFree)
+			if (($CustomVolumePercentFree -le $c) -and ($Volume.SharedVolumeInfo.Partition.FreeSpace -le 1073741824)) {
+				if (($Message -eq $DefaultMessage) -or ($Message -eq "Warning")) {
+					$Message = "Critical"
+					$Status = 2
+				}
+				$VolumesNeedCriticalAttention += "CSV: $FriendlyVolumeName\$Volume Free Space: $CustomVolumePercentFree%; "
+			} elseif (($CustomVolumePercentFree -le $w) -and ($Volume.SharedVolumeInfo.Partition.FreeSpace -le 2147483648)) {
+				if ($Message -eq $DefaultMessage) {
+					$Message = "Warning"
+					$Status = 1
+				}
+				$VolumesNeedWarningAttention += "CSV: $FriendlyVolumeName\$Volume Free Space: $CustomVolumePercentFree%; "
+			}
+		} 
+	}
+}	
+if ($Status -eq 1) {
+	Write-Host "$Message; $VolumesNeedWarningAttention $ListCSV"
+} else {
+	Write-Host "$Message; $VolumesNeedCriticalAttention $ListCSV"
+}
+exit $Status

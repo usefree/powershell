d09d63e55e2833f7d77dd28503bd2fa95b19cbc2
Aleksandr.Neverov <usefreeit@gmail.com>
2018-11-06 11:31:57 +0200

    add RemoveItemsSImple.ps1

---
+++ RemoveItemsSImple.ps1
@@ -0,0 +1,25 @@
+# remove files script
+param( 
+[string]$DirToRemove 
+)
+
+function RemoveItems {
+param( 
+[string]$DirToRemove )
+
+$Items = Get-Childitem "$DirToRemove" -Recurse 
+    foreach ($Item in $Items) {
+        try {
+            $Item | Remove-Item -Force -Recurse -ErrorAction Stop 
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            Write-host "ErrorMessage: $ErrorMessage"
+            Write-host "FailedItem: $FailedItem"
+        }
+    }
+}
+
+RemoveItems $DirToRemove
+
+
